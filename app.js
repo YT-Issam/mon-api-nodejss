923981a24c87592da0356c32e09e8505a3984ac9
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-03-03 15:43:01 +0100

    Registration user

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.DATABASE_URL)
+    .then(_result => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => console.error(err));
+
 const indexRouter = require("./routes/index");
+const AuthRouter = require("./routes/auth");
 
 app.use(express.json());
 
 app.use("/", indexRouter);
+app.use("/auth", AuthRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
